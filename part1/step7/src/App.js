d9b964d304dbfb1fdc3fe4259e7a91c667029e16
efrenruizrubio <efren282@outlook.es>
2023-01-30 23:26:37 -0600

    :rocket: [FINISHED]: step7 exercise done

--- part1/step7/src/App.js
+++ part1/step7/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	// save clicks of each button to its own state
+	const [good, setGood] = useState(0);
+	const [neutral, setNeutral] = useState(0);
+	const [bad, setBad] = useState(0);
+
+	const Button = ({ name, action }) => (
+		<button type="button" onClick={action}>
+			{name}
+		</button>
+	);
+
+	const handleGoodButton = () => {
+		setGood((oldValue) => oldValue + 1);
+	};
+	const handleNeutralButton = () => {
+		setNeutral((oldValue) => oldValue + 1);
+	};
+	const handleBadButton = () => {
+		setBad((oldValue) => oldValue + 1);
+	};
+
+	return (
+		<div>
+			<h1>give feedback</h1>
+			<Button name={"good"} action={handleGoodButton} />
+			<Button name={"neutral"} action={handleNeutralButton} />
+			<Button name={"bad"} action={handleBadButton} />
+			<h1>statistics</h1>
+			<span>good {good}</span>
+			<br />
+			<span>neutral {neutral}</span>
+			<br />
+			<span>bad {bad}</span>
+			<br />
+			<span>all {good + neutral + bad}</span>
+			<br />
+			<span>average {(good - bad) / (good + neutral + bad)}</span>
+			<br />
+			<span>positive {(good / (good + neutral + bad)) * 100} %</span>
+			<br />
+		</div>
+	);
+};
 
 export default App;
